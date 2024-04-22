0011c622608d0eb720faeb2a04932dfacb9f1977
AnnyaB <anabordin2008@gmail.com>
2024-04-22 14:03:19 -0300

    Adicionar arquivos de projeto.

---
+++ Vetores/Program.cs
@@ -0,0 +1,15 @@
+﻿// programa que lê vetor com 5 números inteiro;
+
+int numero;
+int[] numeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    numero = int.Parse(Console.ReadLine());
+    numeros[i] = numero;
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
---
+++ vetorString/Program.cs
@@ -0,0 +1,30 @@
+﻿// programa que lê vetor com 5 letras;
+
+char[] palavra = new char[10];
+
+for (int i = 0; i < 10; i++)
+{
+    palavra[i] = char.Parse(Console.ReadLine());
+    //pode ser Console.readKey também;
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(palavra[i]);
+}
+
+Console.WriteLine();
+
+int x = 9;
+while (x >= 0)
+{
+    Console.Write(palavra[x]);
+    x--;
+}
+
+Console.WriteLine();
+
+for  (int i = 1; i < 10; i += 2)
+{
+    Console.Write(palavra[i]);
+}
